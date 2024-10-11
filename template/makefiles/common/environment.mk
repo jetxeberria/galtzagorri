8536a38b2d9f3544acb76567b564c34ba41951ad
jetxeberria <etxeberria_92@hotmail.com>
2024-10-11 19:14:15 +0200

    Restructure makefile

--- template/makefiles/common/environment.mk
+++ template/makefiles/common/environment.mk
@@ -9,34 +9,50 @@ setup-system:
 	@echo "System setup is complete."
 
 # Target to set up the repository environment (run each time repo is cloned)
-setup-repo: create-env
-	@echo "Repository environment is set up."
+setup-repo: _init-git-repository _create-env
+	@echo "Repository environment is set up. Activate it with 'source .venv/bin/activate'."
 
-create-env:
+_init-git-repository:
+	@echo "Initizalizing the repository..."
+	git init
+
+_create-env: _check-no-env
 	@echo "Creating and activating the PDM environment..."
 	$(PDM) install
 
-# Target to update the repository environment with remote changes (run each time after pulling changes)
-update-repo: verify-env update-template update-env
-	@echo "Repository environment is updated."
-
-verify-env:
-	@echo "Verifying the local environment is activated"
-	@if [ "$$(which python)" != "$$(pdm info --python)" ]; then \
-		echo "Error: Local PDM environment is not activated!"; \
+_check-no-env:
+	@echo "Checking no environment is activated..."
+	@if [ -n "$${VIRTUAL_ENV}" ]; then \
+		echo "\nError: Environment is already activated!"; \
+		echo "Please deactivate the current environment and try again.\n"; \
 		exit 1; \
 	fi
+	@echo "OK"
 
-update-template:
+# Target to update the repository environment with remote changes (run each time after pulling changes)
+update-repo: _check-env-ok _update-template _update-env
+	@echo "Repository environment is updated."
+
+_check-env-ok:
+	@echo "Checking if the correct environment is activated..."
+	@PROJECT_NAME=$$(basename "$$(pwd)"); \
+	ENV_NAME=$$(basename "$$(dirname "$${VIRTUAL_ENV}")"); \
+	if [ "$$ENV_NAME" != "$$PROJECT_NAME" ]; then \
+		echo "\nError: A different environment is activated ($$ENV_NAME) instead of expected ($$PROJECT_NAME)!"; \
+		echo "Please deactivate the current environment, activate the correct environment and try again.\n"; \
+		exit 1; \
+	fi
+	@echo "OK"
+
+_update-template:
 	@echo "Updating the Copier template..."
 
-	
-update-env:
+_update-env:
 	@echo "Updating the local environment with new dependencies..."
 	$(PDM) sync
 
 # Targets to update the local environment
-add-package:
+add-package: _check-env-ok
 	@package_name=$${package_name}; \
 	if [ -z "$$package_name" ]; then \
 		if [ -z "$1"]; then \
@@ -49,12 +65,12 @@ add-package:
 	$(PDM) add $$package_name
 	@echo "Package $$package_name added."
 
-remove-package:
+remove-package: _check-env-ok
 	@read -p "Enter package name to remove: " package_name; \
 	$(PDM) remove $$package_name
 	@echo "Package $$package_name removed."
 
-update-package:
+update-package: _check-env-ok
 	@read -p "Enter package name to update: " package_name; \
 	$(PDM) update $$package_name
 	@echo "Package $$package_name updated."
